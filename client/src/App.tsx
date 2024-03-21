e9ca37edd914270452bb1ee09005b4d16cfd06e1
hhnssl <losnah5h@gmail.com>
2024-03-21 14:46:15 +0900

    feature - 투두 추가 API 변경 (REST -> GraphQL)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
 import TodoItem from "./components/TodoItem";
 import AddTodo from "./components/AddTodo";
-import { getTodos, addTodo, updateTodo, deleteTodo, getTodosGql } from "./API";
+import {
+  getTodos,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+  getTodosGql,
+  addTodoGql,
+} from "./API";
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
@@ -11,10 +18,12 @@ const App: React.FC = () => {
   }, []);
 
   const fetchTodos = (): void => {
+    // REST API
     // getTodos()
     //   .then(({ data: { todos } }: ITodo[] | any) => setTodos(todos))
     //   .catch((err: Error) => console.log(err));
 
+    // GraphQL API
     getTodosGql()
       .then(({ data: { todos } }: ITodo[] | any) => setTodos(todos))
       .catch((err: Error) => console.log(err));
@@ -22,14 +31,15 @@ const App: React.FC = () => {
 
   const handleSaveTodo = (e: React.FormEvent, formData: ITodo): void => {
     e.preventDefault();
-    addTodo(formData)
-      .then(({ status, data }) => {
-        if (status !== 201) {
-          throw new Error("Error! Todo not saved");
-        }
-        setTodos(data.todos);
-      })
-      .catch((err) => console.log(err));
+    addTodoGql(formData);
+    // addTodo(formData)
+    // .then(({ status, data }) => {
+    // if (status !== 201) {
+    // throw new Error("Error! Todo not saved");
+    // }
+    // setTodos(data.todos);
+    // })
+    // .catch((err) => console.log(err));
   };
 
   const handleUpdateTodo = (todo: ITodo): void => {
